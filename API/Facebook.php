e50f01174ad5b5c728c462aeca043e3e2d66337c
Tomasz Borys <tobo@brs-software.pl>
2016-02-19 15:06:09 +0100

    changed link to the post

--- API/Facebook.php
+++ API/Facebook.php
@@ -67,7 +67,8 @@ class Facebook extends AbstractAPI
             ->setContent($rawData->getField('message'))
             ->setDate($rawData->getField('created_time')->format('Y-m-d H:i:s'))
             ->setUserName($rawData->getField('admin_creator')->getField('name'))
-            ->setPostUrl($rawData->getField('link'))
+            ->setPostUrl('https://www.facebook.com/VivitarOfficial/posts/' . explode('_', $rawData->getField('id'))[1])
+            //->setPostUrl($rawData->getField('link'))
             ->setImgUrl($rawData->getField('full_picture'))
         ;
         return $post;
